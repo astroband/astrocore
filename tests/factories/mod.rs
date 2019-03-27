e2c9ad9f8b5740a52a73797a6f31bac7dfc59a50
A.A.Abroskin <arkweid@evilmartians.com>
2019-03-28 00:11:47 +0300

    add deserialize test

--- tests/factories/mod.rs
+++ tests/factories/mod.rs
@@ -1,2 +1,11 @@
 pub mod external_xdr;
 pub mod internal_xdr;
+
+use serde::ser::Serialize;
+
+pub fn prepare<T: Serialize>(rust_struct: T, ruby_struct: String) -> (Vec<u8>, Vec<u8>) {
+    let rust_xdr = internal_xdr::serialize(rust_struct);
+
+    let ruby_xdr = external_xdr::serialize(ruby_struct);
+    (rust_xdr, ruby_xdr)
+}
