bdfdd124fa7bff97a332bdeff19cf153c4061492
A.A.Abroskin <arkweid@evilmartians.com>
2019-04-19 12:25:06 +0300

    update lib.rs to rust2018 style

--- src/lib.rs
+++ src/lib.rs
@@ -1,42 +1,13 @@
-// cryptography
-extern crate base32;
-extern crate base64;
-extern crate bigdecimal;
-extern crate byteorder;
-extern crate crc16;
-extern crate num_bigint;
-extern crate num_traits;
-extern crate sodiumoxide;
-
-// logging
-extern crate env_logger;
-extern crate log;
-
-// database
 #[macro_use]
 extern crate diesel;
-extern crate dotenv;
 
-// serealizer
-extern crate serde;
-extern crate serde_bytes;
-extern crate serde_derive;
-extern crate serde_xdr;
-extern crate toml;
-
-// test
-extern crate simulacrum;
-
-// utils
-extern crate itertools;
-extern crate lazy_static;
-
-pub mod config;
-pub mod crypto;
-pub mod database;
-pub mod factories;
-pub mod network;
-pub mod overlay;
-pub mod schema;
-pub mod scp;
 pub mod xdr;
+pub mod factories;
+
+pub(crate) mod config;
+pub(crate) mod crypto;
+pub(crate) mod database;
+pub(crate) mod network;
+pub(crate) mod overlay;
+pub(crate) mod schema;
+pub(crate) mod scp;
